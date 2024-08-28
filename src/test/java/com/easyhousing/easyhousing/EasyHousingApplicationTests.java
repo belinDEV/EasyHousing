4f75df3757716cb2e983d35601f8275499bd371d
belinDEV <orlando2eb@gmail.com>
2024-08-28 13:51:04 -0300

    primeiro commit

---
+++ src/test/java/com/easyhousing/easyhousing/EasyHousingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyhousing.easyhousing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyHousingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
